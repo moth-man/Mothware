6263fab8b613cfefb806104c137b7f0d308dfd66
Moth-Man <moth-man@Moth-Mans-MacBook-Air.local>
2018-12-27 22:24:47 -0500

    Identify tokens. Print motor object token and print token.

--- LexicalAnalyzer.js
+++ LexicalAnalyzer.js
@@ -2,10 +2,43 @@ var file = process.argv[2];
 
 function readFile(){
     if(file.substr(-3,3) === ".mw"){
+        fileLex(file);
         console.log(file);
     }
     else {
         console.log("ERROR: Incorrect file extension.")
+    }
+}
+
+function fileLex(f){
+    const fs = require('fs')
+    const contents = fs.readFileSync(f, 'utf8')
+    var tape = []
+    var token = ''
+
+    for(let j = 0; j < contents.length; j++){
+        token += contents[j]
+        token.trim()
+        console.log(token)
+
+        if(token === 'motor'){
+            console.log("MOTOR")
+            tape.push['M']
+            token = ''
+        }
+
+        else if(tape.pop() === 'M' && token[j-1] === ";"){
+            console.log("MOTOR VARIABLE")
+            tape.push['MV']
+            token = ''
+        }
+
+        else if(token === 'print'){
+            console.log("PRINT")
+            tape.push['P']
+            token = ''
+        }
+
     }
 }
 
