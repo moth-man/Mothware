12e1127d3b72a92527ca454d881c458b86fd973f
moth-man <mothmanor@gmail.com>
2018-12-29 20:33:27 -0500

    Edit example mothware file to include for-loop. Introduction of new tokens.

--- LexicalAnalyzer.js
+++ LexicalAnalyzer.js
@@ -20,6 +20,10 @@ function fileLex(f){
     var p = 0
     var s = 0
     var sem = 0
+    var f = 0
+    var lp = 0
+    var i = 0
+    var iv = 0
 
     for(let j = 0; j < contents.length; j++){
         token += contents[j]
@@ -52,7 +56,7 @@ function fileLex(f){
 
         else if(token === 'print'){
             console.log("PRINT")
-            tape['P'] = token
+            tape['P' + p] = token
             token = ''
         }
 
@@ -62,16 +66,71 @@ function fileLex(f){
 
         else if(token.charAt(token.length-1) === '"' && openQuote === true){
             console.log("STRING")
-            tape['S'] = token
+            tape['S' + s] = token
+            s++
             token = ''
             openQuote = false
+        }
+
+        else if(token === "for"){
+            console.log("FOR-LOOP")
+            tape['F' + f] = token
+            f++
+            token = ''
+        }
+
+        else if(token === "("){
+            console.log("LEFT-PARENTHESIS")
+            tape['LP' + lp] = token
+            lp++
+            openParenthesis = true
+            token = ''
+        }
+
+        else if(token === "int"){
+            console.log("INTEGER")
+            tape['I' + i] = token
+            i++
+            token = ''
+        }
+
+        else if(tape[Object.size(tape)-1] === "int"){
+            console.log("INTEGER VARIABLE")
+            tape['IV' + iv] = token
+            iv++
+            token = ''
+        }
+
+        else if(token === '='){
+            console.log("EQUALS")
+            tape['EQ' + eq] = token
+            eq++
+            token = ''
         }
     }
 
     console.log(tape)
+    console.log(valueOf())
+    console.log("test")
     for(var i in tape){
         console.log(i + ": " + tape[i])
     }
+}
+
+function valueOf(array,val){
+    for(var count in array){
+        if(count === val){
+            return array[count]
+        }
+    }
+}
+
+Object.size = function(tape){
+    var size = 0
+    for(var q in tape){
+        size++
+    }
+    return size
 }
 
 readFile();
